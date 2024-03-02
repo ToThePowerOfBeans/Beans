c16fa3aa5d49ea89ef0d44896a8acada0a3b6326
ToThePowerOfBeans <ignas.budzinskas@ktu.edu>
2024-03-02 15:39:33 +0200

    Customization scenes linked to main menu and movement scenes

--- Scenes/CharacterCustomization/CharacetrCustomization.gd
+++ Scenes/CharacterCustomization/CharacetrCustomization.gd
@@ -4,11 +4,11 @@ func _on_previuos_pressed():
 	pass # Replace with function body.
 
 func _on_next_pressed():
-	pass # Replace with function body.
+	pass
 
 
 func _on_select_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Scenes/Player.tscn")
 
 
 func _on_back_pressed():

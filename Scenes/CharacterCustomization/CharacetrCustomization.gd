7dfcdb63e6e7d2a389b05c273f2479ed7bc49023
JusteliZz <j.skiparius@gmail.com>
2024-03-02 15:32:05 +0200

    Character Customization

---
+++ Scenes/CharacterCustomization/CharacetrCustomization.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+func _on_previuos_pressed():
+	pass # Replace with function body.
+
+func _on_next_pressed():
+	pass # Replace with function body.
+
+
+func _on_select_pressed():
+	pass # Replace with function body.
+
+
+func _on_back_pressed():
+	get_tree().change_scene_to_file("res://Scenes/NameSelect/NameSelect.tscn")
